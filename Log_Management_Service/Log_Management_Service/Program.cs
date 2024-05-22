143c1252e2e6bf68044a11adfc56ab56b1e1e02a
Manuja Dewmina <manuja1bc@gmail.com>
2024-05-22 16:54:45 +0530

    Update Swagger to development environment

--- Log_Management_Service/Log_Management_Service/Program.cs
+++ Log_Management_Service/Log_Management_Service/Program.cs
@@ -21,11 +21,11 @@ builder.Services.AddDbContext<LogDbContext>(options => options.UseSqlServer(logC
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-// }
+}
 
 app.UseHttpsRedirection();
 
