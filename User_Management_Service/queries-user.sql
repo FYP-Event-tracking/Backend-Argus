80459fdf541272f189ae5a90cdbee1e416cc96fe
Hansa-Hettiarachchi <hansahettiarachchi@gmail.com>
2024-07-09 02:19:18 +0530

    Update query files

--- User_Management_Service/queries-user.sql
+++ User_Management_Service/queries-user.sql
@@ -15,9 +15,25 @@ CREATE TABLE userdata (
 );
 GO
 
+INSERT INTO userdata (userId, userName, userType, userAddress, userTelephone)
+VALUES ('User1', 'Employee', 'User', '123 User St', '123-456-7890');
+GO
+
+INSERT INTO userdata (userId, userName, userType, userAddress, userTelephone)
+VALUES ('Admin1', 'Administrator', 'Admin', '456 Admin Rd', '987-654-3210');
+GO
+
 CREATE TABLE userlogin (
     userId NVARCHAR(191) PRIMARY KEY,
     userType NVARCHAR(191) NOT NULL,
     userPassword NVARCHAR(191) NOT NULL,
 );
+GO
+
+INSERT INTO userlogin (userId, userType, userPassword)
+VALUES ('User1', 'User', '123');
+GO
+
+INSERT INTO userlogin (userId, userType, userPassword)
+VALUES ('Admin', 'Admin', '123');
 GO
